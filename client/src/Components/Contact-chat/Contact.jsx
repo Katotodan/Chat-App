55a8bf11bca516aa304f0a81a5c837c545baa10a
DANIEL KATOTO <danielkatoto@DANIELs-MacBook-Pro.local>
2025-07-15 18:04:43 +0300

    fix: handleClick outside of the side menu

--- client/src/Components/Contact-chat/Contact.jsx
+++ client/src/Components/Contact-chat/Contact.jsx
@@ -1,5 +1,4 @@
 import {useEffect, useState, useRef} from "react";
-import axios from "axios";
 import { SingleContact } from "./SingleContact";
 import { SearchContact } from "./SearchContact";
 import { socket } from "../../socket";
@@ -10,6 +9,7 @@ export const ContactChat = ({setDestination , setDestinationName, currentUserId}
     const [contacts, setContact] = useState([])
     const menuContainer = useRef(null)
     const [isMenuIcon, setIsMenuIcon]  = useState(true)
+
 
     useEffect( () => {
         const conversations = async() =>{
@@ -25,8 +25,20 @@ export const ContactChat = ({setDestination , setDestinationName, currentUserId}
             emitter.off('updateContactList', conversations)
         };
     }, []) 
+
+    useEffect(()=>{
+        const handleClickOutside = (event) => {
+            if(event.target.className !== 'side-menu'){
+                if (menuContainer.current && !menuContainer.current.contains(event.target)) {
+                    setIsMenuIcon(true)
+                }
+            }  
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, [])
     
-    const displayMenu = (e)=>{ isMenuIcon ? setIsMenuIcon(false) : setIsMenuIcon(true)}
+    const displayMenu = (e)=> setIsMenuIcon(prev => !prev)
     const displayMsg = (element) =>{
         setDestination(element.id)
         setDestinationName(element.username)
@@ -45,19 +57,19 @@ export const ContactChat = ({setDestination , setDestinationName, currentUserId}
     
 
     return(
-        <>
+        <aside ref={menuContainer} >
         <div className="h-6 md:hidden absolute top-0 left-2 z-10" onClick={displayMenu} >
-                {isMenuIcon ? <span className="text-2xl cursor-pointer">&#8801;</span>: 
-                <span className="text-red-600 bg-gray-100 text-2xl  w-full cursor-pointer">&#10006;</span>}
+                {isMenuIcon ? <span className="text-2xl cursor-pointer side-menu">&#8801;</span>: 
+                <span className="text-red-600 bg-gray-100 text-2xl  w-full cursor-pointer side-menu">&#10006;</span>}
         </div>
         <div className={ `bg-gray-100 pt-8 md:pt-0 w-screen md:w-full h-[calc(100dvh-6.5rem)] md:h-[calc(100dvh-5rem)] 
-        overflow-y-auto overflow-x-hidden ${isMenuIcon ? "hidden md:block" : ""}`} ref={menuContainer}>
+        overflow-y-auto overflow-x-hidden ${isMenuIcon ? "hidden md:block" : ""}`}>
             <SearchContact updateContact = {updateContact} currentUserId= {currentUserId} />
             {
                 contact.length > 0 ? <>{contact}</> :
                 <span className="block m-5 text-lg text-center">There were no conversations found!</span> 
             }   
         </div>
-        </>
+        </aside>
     )
 }
